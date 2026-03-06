aa7cfe0fca354cf0b18a5ed6d6241b04683c8a35
Will Harris <willmharris7@gmail.com>
2026-03-05 21:54:36 -0800

    moved text-center to container div

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import { Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid text-center">
       <Row>
-        <Col xs={12} lg={6} className="text-center py-5">
+        <Col xs={12} lg={6} className="py-5">
           <p style={{fontSize: '4rem'}}>Hello! I'm Will</p>
         </Col>
-        <Col xs={12} lg={6} className="text-center py-3">
+        <Col xs={12} lg={6} className="py-3">
           <img src={Headshot} style={{width: '17rem', height: '17rem'}} />
         </Col>
       </Row>
       <Row>
-        <Col xs={12} className="text-center py-3">
+        <Col xs={12} className="py-3">
           <p>
             For the past five years I've been a Digital Operations Manager at Adidas, focusing on website content and product data management.
             I facilitate communication between business and development teams here in Portland, OR and across the globe.
@@ -25,23 +25,23 @@ function App() {
         </Col>
       </Row>
       <Row>
-        <Col xs={12} lg={4} className="text-center py-3">
+        <Col xs={12} lg={4} className="py-3">
           <p>Contact:</p>
           <p>william.harris@externals.adidas.com</p>
           <p>willmharris7@gmail.com</p>
         </Col>
-        <Col xs={12} lg={4} className="text-center py-3">
+        <Col xs={12} lg={4} className="py-3">
           <p>Links:</p>
           <p><a href="https://www.linkedin.com/in/willharris7" target="_blank" rel="noreferrer">Linkedin</a></p>
           <p><a href="https://github.com/willmharris7" target="_blank" rel="noreferrer">Github</a></p>
         </Col>
-        <Col xs={12} lg={4} className="text-center py-3">
+        <Col xs={12} lg={4} className="py-3">
           <p>Projects:</p>
           <p>BetterEvents: An improved user interface for Meetup and Eventbrite</p>
         </Col>
       </Row>
       <Row>
-        <Col xs={12} className="text-center py-3">This site is made with React and Bootstrap. Hosted on Vercel.</Col>
+        <Col xs={12} className="py-3">This site is made with React and Bootstrap. Hosted on Vercel.</Col>
       </Row>
     </div>
   )
