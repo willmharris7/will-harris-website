a74d6fed36f5a72ecdc5dc80373f8330efbe1af8
Will Harris <willmharris7@gmail.com>
2026-03-06 10:47:35 -0800

    tutorial text in place

--- src/BetterEvents.tsx
+++ src/BetterEvents.tsx
@@ -1,10 +1,35 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import { Row, Col } from 'react-bootstrap'
 
 function BetterEvents() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <h1>Hello World</h1>
+    <div className="container-fluid text-center fs-4">
+      <Row data-id="tutorial">
+        <Row>
+          <p>
+            Have you ever stared at a blank calendar wondering what to do with your weekend?
+            BetterEvents is an open-source web scraping application that improves the functionality of Meetup.com and Eventbrite.com to help you get out the door.
+          </p>
+        </Row>
+        <Row>
+          <p>Here's what it does:</p>
+          <ul style={{listStylePosition: 'inside'}}>
+            <li>Consolidates all events between Meetup and Eventbrite in one place</li>
+            <li>Adds a start time filter so you can see what events fit your schedule</li>
+            <li>Adds a block button so you can ignore events you're not interested in</li>
+          </ul>
+        </Row>
+        <Row>
+          <p>
+            Your blocklist is stored in your cookies, so please note this app will not work properly in Incognito mode.
+            Currently only Portland is supported, but if you want to add another city just shoot me an email.
+          </p>
+        </Row>
+      </Row>
+      <Row data-id="ui"></Row>
+      <Row data-id="get-events"></Row>
+      <Row data-id="meetup"></Row>
+      <Row data-id="eventbrite"></Row>
     </div>
   )
 }
