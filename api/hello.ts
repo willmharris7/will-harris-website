cf6bd556f67a8525c16da942d0c8379a63d31b48
Will Harris <willmharris7@gmail.com>
2026-03-06 23:24:32 -0800

    browserless basic fetch test working and vercel secrets for api key working

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,14 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // remove later
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
+const BROWSERLESS_API_KEY = process.env.BROWSERLESS_API_KEY;
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    const response = await fetch('https://example.com');
+    const response = await fetch(`https://chrome.browserless.io/content?token=${BROWSERLESS_API_KEY}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ url: 'https://example.com' }),
+    });
+    console.log(response)
     const html = await response.text();
     res.status(200).send(html);
 }
