5cfeab9f429e06f8e8bcd7a369f1501260eebc74
Will Harris <willmharris7@gmail.com>
2026-03-07 11:53:44 -0800

    browserless meetup ping working and pulling out all relevant card data for 1 page

--- api/scrapeMeetup.ts
+++ api/scrapeMeetup.ts
@@ -1,4 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import * as cheerio from 'cheerio';
 
 const BROWSERLESS_API_KEY = process.env.BROWSERLESS_API_KEY;
 
@@ -10,10 +11,19 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const response = await fetch(`https://chrome.browserless.io/content?token=${BROWSERLESS_API_KEY}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ url: `https://www.meetup.com/find/?source=EVENTS&eventType=inPerson&location=us--or--${city}&customStartDate=${date}T03%3A00%3A00-05%3A00&customEndDate=${next_day_iso}T03%3A59%3A59-04%3A00&distance=twentyFiveMiles`}),
+        body: JSON.stringify({ url: `https://www.meetup.com/find/?source=EVENTS&eventType=inPerson&location=us--or--${city}&customStartDate=${date}T03%3A00%3A00-05%3A00&customEndDate=${next_day_iso}T03%3A59%3A59-04%3A00&distance=twentyFiveMiles`, waitForSelector: { selector: `a[data-event-label="Event Card"]` } }),
     });
     const html = await response.text();
-    res.status(200).send(html);
+    const document = cheerio.load(html);
+    const card_data = document('a[data-event-label="Event Card"]').map((_, a) => ({
+        href: document(a).attr('href'),
+        img: document(a).find('img').attr('src'),
+        title: document(a).find('h3').text(),
+        time: document(a).find('time').text(),
+        group: document(a).find('div.flex-shrink.min-w-0.truncate').text(),
+        attendees: document(a).find('span.ds2-m14.py-ds2-8').text()
+    })).get();
+    res.status(200).json(card_data);
 }
 
 
